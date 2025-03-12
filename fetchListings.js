02354b2396062e13a62ef45d618bab566ad6ad02
ebrakumobuku <BUKUBELIEVE3@GMAIL.COM>
2025-03-12 11:39:39 +0100

    Delete fetchListings.js

--- fetchListings.js
+++
@@ -1,7 +0,0 @@
-fetch("/.netlify/functions/fetchListings")
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("listings-container").innerHTML = data
-            .map(listing => `<div><h3>${listing.channel_name}</h3><a href="${listing.channel_link}">Join</a></div>`)
-            .join("");
-    });
